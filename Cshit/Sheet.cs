9ab1ee8f548615460f1278d77cb9e08b5f470144
thisisjose <138240334+thisisjose@users.noreply.github.com>
2023-10-26 22:11:31 -0700

    Update Sheet.cs

--- Cshit/Sheet.cs
+++ Cshit/Sheet.cs
@@ -8,6 +8,36 @@ namespace PruebaM.Cshit
 {
     internal class Sheet
     {
-
+        public void Burbuja()
+        {
+            int n;
+            Console.WriteLine("Que tan grande master");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] num = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("escribeme el numero we", (i+1));
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0; i<n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int vec;
+                    if (num[i] > num[j])
+                    {
+                        vec = num[i];
+                        num[i] = num[j];
+                        num[j] = vec;
+                    }
+                }
+            }
+            Console.WriteLine("Estos son: ");
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
     }
 }
